9f6a9b8f0b41f42a4dbeff08a0beafe6d629b92a
Morasakibara <adriannchare@gmail.com>
2025-06-21 14:08:55 +0100

    ajout du code des models

--- app/Models/Entreprise.php
+++ app/Models/Entreprise.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Entreprise extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'adresse',
+        'telephone',
+        'email',
+    ];
+
+    public function dossiers()
+    {
+        return $this->hasMany(Dossier::class);
+    }
 }
--- app/Models/HistoriqueAction.php
+++ app/Models/HistoriqueAction.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class HistoriqueAction extends Model
 {
     use HasFactory;
+
+    protected $table = 'historique_actions';
+
+    protected $fillable = [
+        'dossier_id',
+        'user_id',
+        'action',
+        'description',
+    ];
+
+    public function dossier()
+    {
+        return $this->belongsTo(Dossier::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFormattedActionAttribute()
+    {
+        $actions = [
+            'CREATE' => 'Création',
+            'UPDATE' => 'Modification',
+            'ASSIGN' => 'Attribution',
+            'ARCHIVE' => 'Archivage',
+            'DOCUMENT_UPLOAD' => 'Ajout de document',
+            'DOCUMENT_DELETE' => 'Suppression de document',
+            'STATUS_CHANGE' => 'Changement de statut',
+        ];
+
+        return $actions[$this->action] ?? $this->action;
+    }
+
 }
